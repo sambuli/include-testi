62a026a13ac021d0470b953e837c6cae9870084f
sambuli <90317527+sambuli@users.noreply.github.com>
2022-02-10 10:28:28 +0200

    testailua
    Lisäsin sisältöä includella

---
+++ footer.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fin">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer</title>
+</head>
+<body>
+    <h2>Täältä löytyy yhteystiedot ja nää</h2>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fin">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Include-testi</title>
+</head>
+<body>
+    <?php include 'header.php'; ?>
+    <h1>Tässä on indexin pääsisältö</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus quasi voluptatibus neque aliquid. Consequatur pariatur quaerat ipsam iure inventore quis voluptatum nemo odit asperiores. Sunt magnam itaque placeat error voluptas.</p>
+    <?php include 'footer.php'; ?>
+</body>
+</html>
